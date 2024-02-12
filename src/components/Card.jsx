a15aa965f2c58d5ca214023deb7a893ecbc250f3
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2024-02-12 02:59:14 +0100

    truncate text

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
+
 function Card(props) {
-  return(
+
+  const description = props.description
+
+  const truncateDescription = (description) => {
+    // Check if the description length is greater than 370 characters
+    if (description.length > 370) {
+      // Truncate the description and add ellipsis
+      return description.substring(0, 370) + '...';
+    }
+    return description; // Return the original description if it's shorter
+  };
+
+  return (
     <section className="card">
       <div className="card-img">
         <img src={props.imageUrl} />
@@ -16,7 +29,8 @@ function Card(props) {
 
         <h1>{props.title}</h1>
         <p className="dates">{props.startDate} - {props.endDate}</p>
-        <p className="description">{props.description}</p>
+        <p className="description">{truncateDescription(props.description)}</p>
+
       </div>
     </section>
   )
